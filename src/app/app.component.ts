966debc18531ed31e927c91640278dea5f8277ab
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-18 21:11:00 +0200

    App component update [WIP]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    PolymerElement('app-header-layout'),
+    PolymerElement('app-header'),
+    PolymerElement('app-toolbar'),
+    PolymerElement('paper-icon-button')
+  ],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  isInChildView = false;
+
+
 }
