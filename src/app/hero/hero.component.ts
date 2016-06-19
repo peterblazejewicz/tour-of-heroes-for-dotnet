943448b37e70ec7ea97613a14bac3b8fe68437bf
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-19 14:19:18 +0200

    Hero component details impl.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, } from '@angular/core';
+import { ISubscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -6,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'hero.component.html',
   styleUrls: ['hero.component.css']
 })
-export class HeroComponent implements OnInit {
 
-  constructor() {}
+export class HeroComponent implements OnInit, OnDestroy {
+
+  private subscription: ISubscription;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(params => {
+      let id = parseInt(params['id'], 10);
+      // todo service call
+      // this.service
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 
 }
