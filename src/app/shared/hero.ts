34e47e57f799605e466f2adc6bd4d91585250387
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-19 17:39:54 +0200

    Hero model class

---
+++ src/app/shared/hero.ts
@@ -0,0 +1,18 @@
+/**
+ * (description)
+ *
+ * @export
+ * @class Hero
+ */
+export class Hero {
+
+  /**
+   * Creates an instance of Hero.
+   *
+   * @param {number} id (description)
+   * @param {string} name (description)
+   * @param {string} birthday (description)
+   */
+  constructor(public id?: number, public name?: string, public birthday?: string) { }
+
+}
