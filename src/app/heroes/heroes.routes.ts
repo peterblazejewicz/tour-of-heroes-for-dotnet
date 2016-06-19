0db849f148c85c719a0e02abc998f599b5bd9e6b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-19 14:19:32 +0200

    Fix missing default route

--- src/app/heroes/heroes.routes.ts
+++ src/app/heroes/heroes.routes.ts
@@ -3,6 +3,6 @@ import { HeroesComponent } from './heroes.component';
 import { HeroComponent } from '../hero/hero.component';
 
 export const HeroesRoutes: RouterConfig = [
-  { path: '/heroes', component: HeroesComponent },
+  { path: '/heroes', component: HeroesComponent, index: true },
   { path: '/hero/:id', component: HeroComponent }
 ]
