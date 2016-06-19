e10d47db4e192199d92b197e3e76c2b02caf257b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-19 19:46:50 +0200

    Prepare JSON serialization settings for Web API

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace TourOfHeroes
 {
@@ -29,7 +31,13 @@ namespace TourOfHeroes
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    o.SerializerSettings.Formatting = Formatting.Indented;
+                    o.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
